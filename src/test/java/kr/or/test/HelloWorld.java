9ea5968efc4cd936ad576f51e899f1ad87840416
ehxhf <ehxhf@DESKTOP-NM7CIT9>
2021-05-27 17:42:10 +0900

    8교시 커밋

--- src/test/java/kr/or/test/HelloWorld.java
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// main은 자바프로그램의 진입 메서드(자동실행) 입니다
-		// classpath-> exclue test code unchecked 해야지 오류가 안남
+		// class path-> exclude test code unchecked 해야지 오류가 안남
 		System.out.println("헬로 자바!");
      
 	}
