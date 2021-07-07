3918d281cc49527cd446ff55903f1be036c5a8f9
ehxhf <ehxhf@DESKTOP-NM7CIT9>
2021-07-07 13:54:44 +0900

    4교시 커밋

--- src/main/java/com/edu/controller/HomeController.java
+++ src/main/java/com/edu/controller/HomeController.java
@@ -16,10 +16,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.edu.service.IF_BoardService;
 import com.edu.service.IF_MemberService;
+import com.edu.util.CommonUtil;
+import com.edu.vo.AttachVO;
 import com.edu.vo.BoardVO;
 import com.edu.vo.MemberVO;
 import com.edu.vo.PageVO;
@@ -51,6 +55,8 @@ public class HomeController {
     private IF_MemberService memberService;
     @Autowired 
     private IF_BoardService boardService;
+    @Inject
+    private CommonUtil commonUtil;
     
     //게시물 리스트 페이지 호출 GET 추가
     @RequestMapping(value="/home/board/board_list",method=RequestMethod.GET)
@@ -74,6 +80,63 @@ public class HomeController {
     	return "home/error/error_404";//.jsp생략
     }
     
+    //MVC구조 기본서식
+    //@RequestMapping 요청 URL값
+    //public 뷰단 jsp파일명을 리턴형식 콜백함수 (자동실행)
+    //return "파일 명";
+    
+    //게시물 상세보기 호출 GET 추가
+    @RequestMapping(value="/home/board/board_view",method=RequestMethod.GET)
+    public String board_view(Model model, @RequestParam("bno")Integer bno,@ModelAttribute("pageVO")PageVO pageVO) throws Exception {
+    	//첨부파일 가져오기
+    	List<AttachVO> listAttachVO = boardService.readAttach(bno);
+    	//첨부파일이 있다면 save_files_names, real_file_names 2개 만듦
+    	String[] save_file_names = new String[listAttachVO.size()];
+    	String[] real_file_names = new String[listAttachVO.size()];
+    	int index= 0;
+    	for(AttachVO file:listAttachVO) { //세로데이터를 가로데이터로 변경처리
+    		
+    		
+    	}
+    	
+    	//db테이블 데이터 가져오기
+    	model.addAttribute("boardVO", boardService.readBoard(bno));
+    	return "home/board/board_view";//.jsp생략
+    }
+    //게시물 등록 처리 POST 추가
+    @RequestMapping(value="/home/board/board_insert",method=RequestMethod.POST)
+    public String board_insert(RedirectAttributes rdat, @RequestParam("file")MultipartFile[] files,BoardVO boardVO) throws Exception {
+    	//첨부파일 처리
+    	String[] save_file_names = new String[files.length];
+    	String[] real_file_names = new String[files.length];
+    	int index = 0;//위 String[]배열의 인덱스 값으로 사용할 변수 선언
+    	for(MultipartFile file:files) {
+    		//첨부파일이 존재하면 실행조건
+    		if(file.getOriginalFilename()!= "") {
+    			real_file_names[index] = file.getOriginalFilename();
+    			save_file_names[index] = commonUtil.fileUpload(file);//UUID를 반환
+    		}
+    		index = index + 1;
+    	}
+    	//Attach테이블에 insert할 첨부파일 가상변수 값을 입력
+    	boardVO.setSave_file_names(save_file_names);
+    	boardVO.setReal_file_names(real_file_names);
+    	//타이틀, contents 내용 시큐어코딩처리
+    	String rawTitle = boardVO.getTitle();
+    	String rawContent = boardVO.getContent();
+    	boardVO.setTitle(commonUtil.unScript(rawTitle));
+    	boardVO.setContent(commonUtil.unScript(rawContent));
+    	//DB테이블처리
+    	boardService.insertBoard(boardVO);
+    	rdat.addFlashAttribute("msg","게시물 등록");//출력 게시물 등록이(가) 성공~
+    	return "redirect:/home/board/board_list";
+    }
+    //게시물 등록 폼 호출 GET추가
+    @RequestMapping(value="/home/board/board_insert_form",method=RequestMethod.GET)
+    public String board_insert_form() throws Exception {
+    	
+    	return "home/board/board_insert";//뷰단.jsp 생략
+    }
     //회원가입 처리 호출 POST방식
     @RequestMapping(value="/join",method=RequestMethod.POST)
     public String join(MemberVO memberVO, RedirectAttributes rdat) throws Exception {
@@ -82,6 +145,8 @@ public class HomeController {
     	String rawPassword = memberVO.getUser_pw();
     	BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     	memberVO.setUser_pw(passwordEncoder.encode(rawPassword));//암호화실행
+    	//사용자레벨은 UI단에서 보내는 값 무시하고 강제로 입력(해킹위험때문에)
+    	memberVO.setLevels("ROLE_USER");
     	memberService.insertMember(memberVO);
     	rdat.addFlashAttribute("msg", "회원등록");//회원가입 가(이) 성공했습니다 출력
     	return "redirect:/login_form";//페이지 리다이렉트로 이동 
